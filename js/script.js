d480203fe22b111c10ca05a2355f03bbdda11fc4
jackhkmatthews <jackhkmatthews@gmail.com>
2016-07-26 16:56:14 +0100

    v4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-/*var html = '';
+//PROBLEM: HAVE TO GENERATE CIRCLES MYSELF - SUPER BORINGGGGGG
+//SOLUTION: GENERATE LOADS OF CIRCELS OF ALL KINDS ON A CANVAS - RANDOMLY!
+
+//random size
+//random position
+//random/escalating z index
+
+var rippleSizeFactor = '20';
+var numberOfSpots = '1';
+var numberOfDarkRipplesPerSpot = 20;
+var numberOfLightRipplesPerSpot = 17;
+var darkRippleOpacityFactor = '200';
+var lightRippleOpacityFactor = '600';
+
+//must match to canvas size in main.css, must be a square
+var canvasHeight = 1000 ;
+
+var html = '';
 var rgbColor;
 var rgbColor1;
 var rgbColor2;
@@ -12,65 +29,72 @@ function getRgbColor () {
   var color = 'rgb(' + getRgbNumber() + ',' + getRgbNumber() + ',' + getRgbNumber() + ')';
   return color;
 }
-  
-for (var i = 0; i < 1; i += 1) {
-  rgbColor1 = getRgbColor();
-  rgbColor2 = getRgbColor();
-  html += '<h2>Top: ' + rgbColor1 + ' to bottom: ' + rgbColor2 + '</h2><div style="background:linear-gradient(' + rgbColor1 + ',' + rgbColor2 + ');"></div>';
-}
-
-document.write(html);
-
-*/
-
-// use setTimeout or setInterval() to keep changing the color
-// automatically every two seconds
-
-
-//PROBLEM: HAVE TO GENERATE CIRCLES MYSELF - SUPER BORINGGGGGG
-//SOLUTION: GENERATE LOADS OF CIRCELS OF ALL KINDS ON A CANVAS - RANDOMLY!
-
-//random size
-//random position
-//random/escalating z index
-
-//make a canvas you like, then add loads of random numbers
-
-var dimensionNumber = '';
-
-function getRgbNumber() {
-  return Math.floor(Math.random() * 256 );
-}
-
-function getRgbColor () {
-  var color = 'rgb(' + getRgbNumber() + ',' + getRgbNumber() + ',' + getRgbNumber() + ')';
-  return color;
-}
-
-function getGradientValue () {
-	var gradientValue;
-	rgbColor1 = getRgbColor();
-    rgbColor2 = getRgbColor();
-    gradientValue = 'linear-gradient(' + rgbColor1 + ',' + rgbColor2 + ')';
-    return gradientValue;
-}
 
 function getPositionNumber () {
-	return Math.floor(Math.random() * 500 );
+        return Math.floor(Math.random() * (canvasHeight + rippleSizeFactor*10) ) - rippleSizeFactor*10;
+      }
+
+
+
+function generateRipples () {
+      for (var i = 0; i < numberOfSpots; i += 1) {
+
+        var rippleId = 'ripple' + i;
+        var rippleSelector = '#ripple' + i;
+
+
+        $("#canvas").append('<div class="ripple" id=' + rippleId + '></div>');
+        $(rippleSelector).css("top", getPositionNumber)
+        $(rippleSelector).css("left", getPositionNumber)
+
+
+        //the outer 5
+
+        for (var j = (numberOfLightRipplesPerSpot + numberOfDarkRipplesPerSpot); j > (numberOfDarkRipplesPerSpot - 1); j -= 1) {
+          var dimension = (j * rippleSizeFactor) + 'px';
+          var opacity = (j / lightRippleOpacityFactor);
+
+          $(rippleSelector).append('<div class="circle"></div>');
+          $(".circle").last().css("height", dimension);
+          $(".circle").last().css("width", dimension);
+          $(".circle").last().css("opacity", opacity);
+
+        }
+
+        //the inner 5
+
+        for (var j = (numberOfDarkRipplesPerSpot + 1); j > 0; j -= 1) {
+          var dimension = (j * rippleSizeFactor) + 'px';
+          var opacity = (j / darkRippleOpacityFactor);
+
+          $(rippleSelector).append('<div class="circle"></div>');
+          $(".circle").last().css("height", dimension);
+          $(".circle").last().css("width", dimension);
+          $(".circle").last().css("opacity", opacity);
+
+        }
+
+        var rippleColor = getRgbColor();
+
+        $(rippleSelector).children().css("background-color", rippleColor)
+
+      }
+
 }
 
-function getDimensionNumber () {
-	return Math.floor(Math.random() * 100 );
-}
+generateRipples();
 
-for (var i = 500; i > 0; i -= 10) {
 
-	dimensionNumber = getDimensionNumber();
-	$("#canvas").append('<div class="circle"></div>');
-	$(".circle").last().css("background", getGradientValue());
-	$(".circle").last().css("top", getPositionNumber());
-	$(".circle").last().css("left", getPositionNumber());
-	$(".circle").last().css("height", i);
-	$(".circle").last().css("width", i);
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
