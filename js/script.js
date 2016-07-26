22a6a1c0804540b16307415d0a9f5a298547253a
jackhkmatthews <jackhkmatthews@gmail.com>
2016-07-26 16:59:42 +0100

    v8

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-/*var html = '';
+//PROBLEM: HAVE TO GENERATE CIRCLES MYSELF - SUPER BORINGGGGGG
+//SOLUTION: GENERATE LOADS OF CIRCELS OF ALL KINDS ON A CANVAS - RANDOMLY!
+
+//random size
+//random position
+//random/escalating z index
+
+var rippleSizeFactor = '10';
+var numberOfSpots = '5';
+var numberOfDarkRipplesPerSpot = 20;
+var numberOfLightRipplesPerSpot = 17;
+var darkRippleOpacityFactor = '200';
+var lightRippleOpacityFactor = '600';
+
+//must match to canvas size in main.css, must be a square
+var canvasHeight = 750 ;
+
+var html = '';
 var rgbColor;
 var rgbColor1;
 var rgbColor2;
@@ -12,65 +29,156 @@ function getRgbColor () {
   var color = 'rgb(' + getRgbNumber() + ',' + getRgbNumber() + ',' + getRgbNumber() + ')';
   return color;
 }
-  
-for (var i = 0; i < 1; i += 1) {
-  rgbColor1 = getRgbColor();
-  rgbColor2 = getRgbColor();
-  html += '<h2>Top: ' + rgbColor1 + ' to bottom: ' + rgbColor2 + '</h2><div style="background:linear-gradient(' + rgbColor1 + ',' + rgbColor2 + ');"></div>';
-}
-
-document.write(html);
-
-*/
-
-// use setTimeout or setInterval() to keep changing the color
-// automatically every two seconds
-
-
-//PROBLEM: HAVE TO GENERATE CIRCLES MYSELF - SUPER BORINGGGGGG
-//SOLUTION: GENERATE LOADS OF CIRCELS OF ALL KINDS ON A CANVAS - RANDOMLY!
-
-//random size
-//random position
-//random/escalating z index
-
-//make a canvas you like, then add loads of random numbers
-
-var dimensionNumber = '';
-
-function getRgbNumber() {
-  return Math.floor(Math.random() * 256 );
-}
-
-function getRgbColor () {
-  var color = 'rgb(' + getRgbNumber() + ',' + getRgbNumber() + ',' + getRgbNumber() + ')';
-  return color;
-}
-
-function getGradientValue () {
-	var gradientValue;
-	rgbColor1 = getRgbColor();
-    rgbColor2 = getRgbColor();
-    gradientValue = 'linear-gradient(' + rgbColor1 + ',' + rgbColor2 + ')';
-    return gradientValue;
-}
 
 function getPositionNumber () {
-	return Math.floor(Math.random() * 500 );
+        return Math.floor(Math.random() * (canvasHeight + rippleSizeFactor*10) ) - rippleSizeFactor*10;
+      }
+
+function generateRipples () {
+      for (var i = 0; i < numberOfSpots; i += 1) {
+
+        var rippleId = 'ripple' + i;
+        var rippleSelector = '#ripple' + i;
+        var rippleColor = getRgbColor();
+        var pulseId = 'pulse' + i;
+        var pulseSelector = '#pulse' + i;
+
+
+        $("#canvas").append('<div class="ripple" id=' + rippleId + '></div>');
+        $(rippleSelector).css("top", getPositionNumber)
+        $(rippleSelector).css("left", getPositionNumber)
+
+
+        //the outer spots
+
+        for (var j = (numberOfLightRipplesPerSpot + numberOfDarkRipplesPerSpot); j > (numberOfDarkRipplesPerSpot - 1); j -= 1) {
+          var dimension = (j * rippleSizeFactor) + 'px';
+          var opacity = (j / lightRippleOpacityFactor);
+
+          $(rippleSelector).append('<div class="circle"></div>');
+          $(".circle").last().css("height", dimension);
+          $(".circle").last().css("width", dimension);
+          $(".circle").last().css("opacity", opacity);
+
+        }
+
+        //the inner spots
+
+        for (var j = (numberOfDarkRipplesPerSpot + 1); j > 0; j -= 1) {
+          var dimension = (j * rippleSizeFactor) + 'px';
+          var opacity = (j / darkRippleOpacityFactor);
+
+          $(rippleSelector).append('<div class="circle"></div>');
+          $(".circle").last().css("height", dimension);
+          $(".circle").last().css("width", dimension);
+          $(".circle").last().css("opacity", opacity);
+
+        }
+
+        $(rippleSelector).children().css("background-color", rippleColor)
+
+        //the animated ripple
+
+        for (var j = 1; j > 0; j -= 1) {
+          var dimension = (j * rippleSizeFactor) + 'px';
+          var opacity = (j / darkRippleOpacityFactor);
+
+          $(rippleSelector).append('<div class="circle pulse" id="' + pulseId + '"></div>');
+          // $(".pulse").last().css("border-style", "solid");
+          // $(".pulse").last().css("border-width", "10px");
+          $(".pulse").last().css("background-color", rippleColor);
+
+        }
+
+      }
+
 }
 
-function getDimensionNumber () {
-	return Math.floor(Math.random() * 100 );
+///generate ripples
+
+generateRipples();
+
+
+//extract number of travelers from each station
+
+for (var i = 0; i < stations.length; i += 1) {
+  var station = stations[i];
+  var numberOfTravelers = 0;
+  for (var j = 0; j < travelers.length; j += 1) {
+    if (station === travelers[j].StartStn) {
+      numberOfTravelers += 1;
+    }
+  }
+  travelersPerStation.push(numberOfTravelers)
 }
 
-for (var i = 500; i > 0; i -= 10) {
+var functionName = stations[1]
 
-	dimensionNumber = getDimensionNumber();
-	$("#canvas").append('<div class="circle"></div>');
-	$(".circle").last().css("background", getGradientValue());
-	$(".circle").last().css("top", getPositionNumber());
-	$(".circle").last().css("left", getPositionNumber());
-	$(".circle").last().css("height", i);
-	$(".circle").last().css("width", i);
-
+function name () {
+  $("#pulse1").animate({
+    opacity: '0.0',
+    height: '2000px',
+    width: '2000px',
+  }, 1000);
+  $("#pulse1").animate({
+    opacity: '0.0',
+    height: '20px',
+    width: '20px',
+  }, 1000);
+  $("#pulse1").animate({
+    opacity: '1',
+    height: '20px',
+    width: '20px',
+  },1000)
 }
+
+
+setInterval(function(){
+
+$("#pulse1").animate({
+    opacity: '0.0',
+    height: '2000px',
+    width: '2000px',
+  }, 1000);
+  $("#pulse1").animate({
+    opacity: '0.0',
+    height: '20px',
+    width: '20px',
+  }, 1000);
+  $("#pulse1").animate({
+    opacity: '1',
+    height: '20px',
+    width: '20px',
+  },1000)
+
+
+}, 3000);
+
+
+setInterval(function(){
+
+$("#pulse0").animate({
+    opacity: '0.0',
+    height: '2000px',
+    width: '2000px',
+  }, 500);
+  $("#pulse0").animate({
+    opacity: '0.0',
+    height: '20px',
+    width: '20px',
+  }, 500);
+  $("#pulse0").animate({
+    opacity: '1',
+    height: '20px',
+    width: '20px',
+  },500)
+
+
+}, 1500);
+
+
+
+
+
+
+
